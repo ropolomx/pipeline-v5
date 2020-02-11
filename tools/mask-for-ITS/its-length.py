d1fb57504834f31be22fab26d22e70f37a2c6965
Varsha <vkale@ebi.ac.uk>
2020-02-11 09:41:46 +0000

    change min length and add empty check

--- tools/mask-for-ITS/its-length.py
+++ tools/mask-for-ITS/its-length.py
@@ -7,24 +7,27 @@ import gzip
 
 def get_avg_length(input_folder):
     masked_its = os.path.join(input_folder, 'ITS_masked.fasta.gz')
-    all_lengths = []
-    with gzip.open(masked_its, "rt") as unzipped_file:
-        for record in SeqIO.parse(unzipped_file, "fasta"):
-            sequences = [x for x in record.seq.split('N') if x and x != '']
-            longest_seq = {'num': 0, 'letters': ''}
-            for seq in sequences:
-                length = len(seq)
-                if length > longest_seq['num']:
-                    longest_seq['num'] = length
-                    longest_seq['letters'] = seq
-            all_lengths.append(longest_seq['num'])
-    return sum(all_lengths)/len(all_lengths)
+    if os.path.exists(masked_its):
+        all_lengths = []
+        with gzip.open(masked_its, "rt") as unzipped_file:
+            for record in SeqIO.parse(unzipped_file, "fasta"):
+                sequences = [x for x in record.seq.split('N') if x and x != '']
+                longest_seq = {'num': 0, 'letters': ''}
+                for seq in sequences:
+                    length = len(seq)
+                    if length > longest_seq['num']:
+                        longest_seq['num'] = length
+                        longest_seq['letters'] = seq
+                all_lengths.append(longest_seq['num'])
+        return sum(all_lengths)/len(all_lengths)
+    else:
+        return 0
 
 
 def suppress_dir(its_length, taxonomy_dir):
     ssu_folder, lsu_folder, its_folder, new_ssu_folder, new_lsu_folder, new_its_folder = \
         [os.path.join(taxonomy_dir, x) for x in ['SSU', 'LSU', 'its', 'suppressed_SSU', 'suppressed_LSU', 'suppressed_its']]
-    if its_length >= 150:
+    if its_length >= 200:
         os.rename(lsu_folder, new_lsu_folder)
         os.rename(ssu_folder, new_ssu_folder)
     else:
@@ -41,5 +44,6 @@ if __name__ == '__main__':
     else:
         args = parser.parse_args()
         avg = get_avg_length(args.seq_directory)
-        print('average ITS length is ' + avg)
-        suppress_dir(avg, args.tax_directory)
+        print('average ITS length is ' + str(avg))
+        if avg > 0:
+            suppress_dir(avg, args.tax_directory)
